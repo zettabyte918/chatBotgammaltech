b8b59490c38eac37f807720deb51aebcd0cbfdf9
Hammami Houssem <hammamihossem99@gmail.com>
2023-03-15 16:25:02 +0100

    update port 3333

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ const button = chatBox.querySelector("button");
 const chatBoxBody = chatBox.querySelector(".chat-box-body");
 
 button.addEventListener("click", sendMessage);
-inputField.addEventListener("keypress", function(event) {
+inputField.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     sendMessage();
   }
@@ -17,36 +17,34 @@ function sendMessage() {
   chatBoxBody.innerHTML += `<div id="loading" class="response loading">.</div>`;
   scrollToBottom();
   window.dotsGoingUp = true;
-    var dots = window.setInterval( function() {
-        var wait = document.getElementById("loading");
-        if ( window.dotsGoingUp ) 
-            wait.innerHTML += ".";
-        else {
-            wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
-        if ( wait.innerHTML.length < 2)
-            window.dotsGoingUp = true;
-        }
-        if ( wait.innerHTML.length > 3 )
-            window.dotsGoingUp = false;
-        }, 250);
+  var dots = window.setInterval(function () {
+    var wait = document.getElementById("loading");
+    if (window.dotsGoingUp) wait.innerHTML += ".";
+    else {
+      wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
+      if (wait.innerHTML.length < 2) window.dotsGoingUp = true;
+    }
+    if (wait.innerHTML.length > 3) window.dotsGoingUp = false;
+  }, 250);
 
-  fetch('http://localhost:3000/message', {
-    method: 'POST',
+  fetch("http://localhost:3333/message", {
+    method: "POST",
     headers: {
-      accept: 'application.json',
-      'Content-Type': 'application/json'
+      accept: "application.json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({message})
-  }).then(response => {
-    return response.json();
-  }).then(data => {
-    document.getElementById("loading").remove();
-    chatBoxBody.innerHTML += `<div class="response"><p>${data.message}</p></div>`;
-    scrollToBottom();
+    body: JSON.stringify({ message }),
   })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.getElementById("loading").remove();
+      chatBoxBody.innerHTML += `<div class="response"><p>${data.message}</p></div>`;
+      scrollToBottom();
+    });
 }
 
 function scrollToBottom() {
   chatBoxBody.scrollTop = chatBoxBody.scrollHeight;
 }
-
